1012794688cbd7f8decba36a4e7367ad3df66756
BeetrootAcademyAccount3 <beetrootacademy1@yahoo.com>
2025-02-06 21:44:16 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -6,27 +6,50 @@ import Login from "./components/Login";
 import CardList from "./components/Cards/CardList";
 import Donations from "./components/Donations/Donations";
 import Header from "../src/components/Header";
-import { Routes, Route, Link, NavLink } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  Link,
+  NavLink,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import AboutUs from "./components/AboutUs";
 import DogDetails from "./components/Cards/Comments/DogDetails";
+import NotFound from "./components/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/aboutUs",
+        element: <AboutUs />,
+      },
+      {
+        path: "/dogs",
+        element: <CardList />,
+        children: [
+          {
+            path: ":id",
+            element: <DogDetails />,
+          },
+        ],
+      },
+      {
+        path: "/donations",
+        element: <Donations />,
+      },
+    ],
+  },
+]);
 
 function App() {
   const isLoggedIn = true;
 
-  return (
-    <div>
-      <Home />
-      <Routes>
-        <Route path="/" element={<AboutUs />} />
-
-        <Route path="/dogs" element={<CardList />}>
-          <Route path=":id" element={<DogDetails />} />
-        </Route>
-
-        <Route path="/donations" element={<Donations />} />
-      </Routes>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,35 +3,38 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { AiOutlineSun, AiFillSun } from "react-icons/ai";
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 function Header({ currentPage, setCurrentPage, darkTheme, toggleTheme }) {
   const isServerDown = false;
   const isAdmin = true;
 
   return (
-    <Navbar
-      bg={darkTheme ? "dark" : "light"}
-      data-bs-theme={darkTheme ? "dark" : "light"}
-    >
-      <Container className="m-0 pl-4">
-        <Navbar.Brand href="#home">Dog Shelter</Navbar.Brand>
-        <Nav className="ms-auto">
-          <Nav.Link as={NavLink} to="dogs">
-            Dogs
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="donations">
-            Donations
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/">
-            About us
-          </Nav.Link>
-          <Nav.Link onClick={toggleTheme}>
-            {darkTheme ? <AiOutlineSun /> : <AiFillSun color="black" />}
-          </Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+    <div>
+      <Navbar
+        bg={darkTheme ? "dark" : "light"}
+        data-bs-theme={darkTheme ? "dark" : "light"}
+      >
+        <Container className="m-0 pl-4">
+          <Navbar.Brand href="#home">Dog Shelter</Navbar.Brand>
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} to="dogs">
+              Dogs
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="donations">
+              Donations
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="aboutUs">
+              About us
+            </Nav.Link>
+            <Nav.Link onClick={toggleTheme}>
+              {darkTheme ? <AiOutlineSun /> : <AiFillSun color="black" />}
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
   );
 }
 
